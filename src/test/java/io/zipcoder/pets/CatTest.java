fa8bb4967b77990c668b3f6b18265c6b545fe476
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-26 19:01:40 -0400

    added CatTest with tests

---
+++ src/test/java/io/zipcoder/pets/CatTest.java
@@ -0,0 +1,23 @@
+package io.zipcoder.pets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by emilybutte on 9/26/16.
+ */
+public class CatTest {
+    Pet c = new Cat("Bob");
+
+    @Test
+    public void speakTest() {
+        assertEquals("meow", c.speak());
+    }
+
+    @Test
+    public void speakOtherTest() {
+        assertNotEquals("blah", c.speak());
+    }
+
+}
