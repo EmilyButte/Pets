9f8619cf560c8b8e2ed597841580f04170cc3665
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-27 10:48:16 -0400

    added tests for dogs and fish

--- src/test/java/io/zipcoder/pets/DogTest.java
+++ src/test/java/io/zipcoder/pets/DogTest.java
@@ -1,11 +1,24 @@
 package io.zipcoder.pets;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by emilybutte on 9/26/16.
  */
 public class DogTest {
+
+    Pet d = new Dog("Bob");
+
+    @Test
+    public void speakTest() {
+        assertEquals("bark", d.speak());
+    }
+
+    @Test
+    public void speakOtherTest() {
+        assertNotEquals("blah", d.speak());
+    }
 
 }
--- src/test/java/io/zipcoder/pets/FishTest.java
+++ src/test/java/io/zipcoder/pets/FishTest.java
@@ -1,9 +1,24 @@
 package io.zipcoder.pets;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by emilybutte on 9/26/16.
  */
 public class FishTest {
 
+    Pet f = new Fish("Bob");
+
+    @Test
+    public void speakTest() {
+        assertEquals("bloop", f.speak());
+    }
+
+    @Test
+    public void speakOtherTest() {
+        assertNotEquals("blah", f.speak());
+    }
 
 }
